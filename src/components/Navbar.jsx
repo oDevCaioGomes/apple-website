83627a3519c3c2df8d431e84c119e8f8c9087dfb
M Khadar Basha Jilan <khadarbashajilan001@gmail.com>
2025-04-11 19:41:51 +0530

    Update Navbar.jsx
    the navbar flex is fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,26 +3,24 @@ import {navLists} from '../constants';
 
 const Navbar = () => {
   return (
-    <header className="w-full py-5 sm:px-10 px-5 flex justify-between 
+    <header className="w-full flex justify-between 
     items-center">
-      <nav className="flex w-full screen-max-width">
+      <nav className="flex w-full">
         <img src={appleImg} alt="Apple" width={14} height={18} />
 
 
 
-        <div className="flex flex-1 justify-center 
+        <a className="flex  justify-center
         max-sm:hidden">
           {navLists.map((nav) => (
-            <div key={nav} className='px-5'>
+            <a key={nav} className='px-5'>
               {nav}
-            </div>
+            </a>
           ))}
-        </div>
+        </a>
 
-        <div>
           <img src={searchImg} alt="search" width={18} height={18} />
           <img src={bagImg} alt="bag" width={18} height={18} />
-        </div>
       </nav>
      </header>
   )
